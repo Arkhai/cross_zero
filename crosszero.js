9d6e78ae2d7d75eebc022c30b6404e974f90ea6b
Riamarh <kiriam@bk.ru>
2019-06-21 21:05:15 +0300

    Update crosszero.js

--- crosszero.js
+++ crosszero.js
@@ -1,5 +1,34 @@
-// JavaScript source code
+var numberCells = 5;
+var arrayOfCells = [];
+var divForCells;
+var mainDivForCells = document.querySelector('.cross_zero');
+
+var root = document.querySelector(':root');
+var rootStyles = getComputedStyle(root);
+var maxWidth = rootStyles.getPropertyValue('--max-width');
+
+function setMaxWidthOfField() {
+    root.style.setProperty('--max-width', (50 + numberCells * 100) + 'px');
+}
+
+setMaxWidthOfField();
+
+for (var j = 0; j < numberCells; j++) {
+    arrayOfCells[j] = [];
+    for (var m = 0; m < numberCells; m++) {
+        arrayOfCells[j][m] = -1;
+        divForCells = document.createElement('div');
+        divForCells.classList.add("block");
+        //      divForCells.className='block';
+        //     divForCells.innerHTML='+';
+        mainDivForCells.appendChild(divForCells);
+    }
+    // console.log(arrayOfCells);
+}
+
+
 var blockToClick = document.querySelectorAll('.block');
+
 
 for (e = 0; e < blockToClick.length; e++) {
     blockToClick[e].addEventListener('click', blockToClickHandler);
